9caceda230a210dff9107b4dcb1642b2b1392b82
Alexey Dolinenko <lekks@yandex.ru>
2021-02-24 23:32:05 +0300

    add some TODO for operators

---
+++ urx/urx_decouple.hpp
@@ -0,0 +1,9 @@
+//
+// Created by ldir on 24.02.2021.
+//
+
+#ifndef URXLIB_URX_DECOUPLE_HPP
+#define URXLIB_URX_DECOUPLE_HPP
+
+
+#endif //URXLIB_URX_DECOUPLE_HPP
---
+++ urx/urx_dsp.hpp
@@ -0,0 +1,29 @@
+//
+// Created by ldir on 24.02.2021.
+//
+
+#ifndef URXLIB_URX_DSP_HPP
+#define URXLIB_URX_DSP_HPP
+
+#include "urx_observer.hpp"
+
+namespace urx {
+
+    /* To Implement:
+     * Distinct filter
+     * Transform (multiply on matrix or polynom)
+     * Exponential filter
+     * Window average
+     * Swinging door filter
+     * Filer small changes
+     * Hysteresis
+     * Integrator
+     * Differentiator
+     * Min
+     * Max
+     * Average
+     * Count
+     */
+
+}
+#endif //URXLIB_URX_DSP_HPP
